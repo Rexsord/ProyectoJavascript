e1a643d68d4a490e9dca5a01a68bb057fe7a3093
unknown <darvis.almonte@hotmail.com>
2017-11-26 12:59:50 -0400

    Integrando CSS

---
+++ resources/scripts/estudiante.js
@@ -0,0 +1,60 @@
+	//<script>
+	var Estudiante = function()
+		{
+			var self = this;
+			self.id = "";
+			self.nombre = "";
+			self.matricula = "";
+			self.identificacion = "";
+			self.telefono = "";
+			self.email = "";
+		};
+	
+	
+	function addRow()
+	{
+	
+		//Objeto Estudiante
+		var est1 = new Estudiante();												//Crear objeto para llenarlo a través del formulario
+		est1.id = document.getElementById("ID").value;								//Obtener los datos del formulario
+		est1.nombre = document.getElementById("Nombre").value;
+		est1.matricula = document.getElementById("Matricula").value;
+		est1.identificacion = document.getElementById("Identificacion").value;
+		est1.telefono = document.getElementById("Telefono").value;
+		est1.email = document.getElementById("Email").value;
+		
+		//Creación de la tabla
+		var table = document.getElementById("estudiantes");							
+		var tr = document.createElement("tr");
+		
+		//Creación y vinculación de los datos
+		var tdId = document.createElement("td");
+		var txtId = document.createTextNode(est1.id);
+		var tdNombre = document.createElement("td");
+		var txtNombre = document.createTextNode(est1.nombre);
+		var tdMatricula = document.createElement("td");
+		var txtMatricula = document.createTextNode(est1.matricula);
+		var tdIdentificacion = document.createElement("td");
+		var txtIdentificacion = document.createTextNode(est1.identificacion);
+		var tdTelefono = document.createElement("td");
+		var txtTelefono = document.createTextNode(est1.telefono);
+		var tdEmail = document.createElement("td");
+		var txtEmail = document.createTextNode(est1.email);
+		
+		//Insercción en la tabla
+		tdId.appendChild(txtId);
+		tr.appendChild(tdId);
+		tdNombre.appendChild(txtNombre);
+		tr.appendChild(tdNombre);
+		tdMatricula.appendChild(txtMatricula);
+		tr.appendChild(tdMatricula);
+		tdIdentificacion.appendChild(txtIdentificacion);
+		tr.appendChild(tdIdentificacion);
+		tdTelefono.appendChild(txtTelefono);
+		tr.appendChild(tdTelefono);
+		tdEmail.appendChild(txtEmail);
+		tr.appendChild(tdEmail);
+		
+		table.appendChild(tr);
+	}
+	//</script>
